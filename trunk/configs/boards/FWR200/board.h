93dc09f1c26cfa29ba50d8c9c6c61256283f4087
doingsths <110985129+doingsths@users.noreply.github.com>
2024-11-21 08:45:25 +0800

    Update board.h

--- trunk/configs/boards/FWR200/board.h
+++ trunk/configs/boards/FWR200/board.h
@@ -1,1 +1,23 @@
+/* FWR200 */
 
+#define BOARD_PID		"FWR200"
+#define BOARD_NAME		"FWR200"
+#define BOARD_DESC		"FAST FWR200 Wireless Router"
+#define BOARD_VENDOR_NAME	"FAST"
+#define BOARD_VENDOR_URL	"http://www.fast.com.cn/"
+#define BOARD_MODEL_URL		"http://www.fast.com.cn/"
+#define BOARD_BOOT_TIME		25
+#define BOARD_FLASH_TIME	120
+#define BOARD_GPIO_BTN_RESET	1
+#undef  BOARD_GPIO_LED_ALL
+#define  BOARD_GPIO_LED_WIFI	72
+#undef  BOARD_GPIO_LED_POWER
+#undef  BOARD_GPIO_LED_LAN
+#undef  BOARD_GPIO_LED_WAN
+#undef  BOARD_GPIO_PWR_USB
+#define BOARD_GPIO_LED_ROUTER	39
+#define BOARD_NUM_ANT_2G_TX	2
+#define BOARD_NUM_ANT_2G_RX	2
+#define BOARD_NUM_ETH_LEDS	1
+#define BOARD_HAS_EPHY_L1000	0
+#define BOARD_HAS_EPHY_W1000	0
